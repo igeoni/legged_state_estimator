80a95031daf18ce2d06c32afd80cbafbfea596fd
igeoni <egune41@gmail.com>
2026-07-22 16:00:38 +0900

    [remove] contact estimator

--- src/data_collector/contact_data_collector_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <rclcpp/rclcpp.hpp>
-#include "legged_state_estimator/data_collector/contact_data_collector.hpp"
-
-int main(int argc, char* argv[]) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<legged_state_estimator::ContactDataCollector>());
-  rclcpp::shutdown();
-  return 0;
-}
